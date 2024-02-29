c038d306f8ca739775712cb053b86ddebcf2f327
cshimm <cameronjshimmin@gmail.com>
2024-02-28 16:45:49 -0800

    order processing system implementation

--- order_processing_system/order_processing_system.py
+++ order_processing_system/order_processing_system.py
@@ -1,3 +1,5 @@
+import unittest
+
 class Order:
     def __init__(self, id, cust_details, order_details) -> None:
         self.id = id
@@ -12,7 +14,7 @@ class ListNode:
 class OrderList:
     def __init__(self, head) -> None:
         self.head = head
-        self.tail = None
+        self.tail = head
 
     def append(self, order):
         self.tail.next = order
@@ -21,17 +23,37 @@ class OrderList:
     def display(self):
         curr = self.head
         while curr:
-            print(curr)
+            print(curr.value.id)
             curr = curr.next
     
     def reverse_helper(self, curr, prev):
-        if curr.next == None:
-            return curr
+        if curr == None:
+            self.head = prev
+            return
         next = curr.next
         curr.next = prev
         previous = curr
-        self.reverse(curr.next, previous)
+        self.reverse_helper(next, previous)
 
     def reverse(self):
         self.reverse_helper(self.head, None)
+        return self
 
+class TestReverseLinkedListRecursion(unittest.TestCase):
+    def test_append(self):
+        order_list = OrderList(ListNode(Order(1,1,1)))
+        for i in range(2, 5):
+            order = Order(i, str(i), str(i))
+            order_list.append(ListNode(order))
+        # order_list.display()
+    
+    def test_reverse(self):
+        order_list = OrderList(ListNode(Order(1,1,1)))
+        for i in range(2, 5):
+            order = Order(i, str(i), str(i))
+            order_list.append(ListNode(order))
+        order_list.reverse()
+        order_list.display()
+        
+if '__main__' == __name__:
+    unittest.main()
